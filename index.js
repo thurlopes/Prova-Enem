79093f1202ec9b6f68286a89ba41f30de3ce58fa
ARTHUR BICIGO LOPES <00001096643984sp@aluno.educacao.sp.gov.br>
2026-06-17 12:27:18 -0300

    enviando a prova

---
+++ index.js
@@ -0,0 +1,13 @@
+function irParaCadastro() {
+    window.location.href = "index.html";
+}
+
+function salvarInscricao() {
+    window.location.href = "cadastro.html";
+}
+
+function voltarParaInicial() {
+    window.location.href = "cadastro.html";
+}
+
+
